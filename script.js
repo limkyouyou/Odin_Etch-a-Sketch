23c1a085027b9072fac79f25c6309ad08ad54174
Kyo young Lim <limkyouyou@gmail.com>
2022-11-30 12:54:37 +0900

    Enable mouse hover color change

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const gridContainer = document.querySelector('#grid-container');
-
-
-
+let gridItem;
 
 for (let i = 0; i < 256; i++) {
   gridContainer.style.cssText = (`grid-template-columns: repeat(16, auto);`);
-  const gridItem = document.createElement('div');
+  gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
   gridContainer.appendChild(gridItem);
-};
+}
+
+const items = document.querySelectorAll('.grid-item');
+
+items.forEach(item => item.addEventListener('mouseover', color));
+
+function color(e) {
+  e.target.style.cssText = (`background-color: black;`);
+}
