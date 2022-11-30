3a4a138a68371c36415c67d4a698bda1a2ea8344
Kyo young Lim <limkyouyou@gmail.com>
2022-11-30 11:20:23 +0900

    Add style to nodes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const gridContainer = document.querySelector('#grid-container');
 const gridItem = document.createElement('div');
+
 gridItem.setAttribute('id', '.grid-item');
+
+gridContainer.style.cssText = ('width: 1000px; height: 1000px; background-color: gray;');
+gridItem.style.cssText = ('background-color: white;');
 
